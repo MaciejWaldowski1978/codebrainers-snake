9cf07fb79f9c8af432af27782f2a0b607bbefa31
Maciej Waldowski <maciejwaldowski@macbook-air-maciej.home>
2019-11-12 21:13:09 +0100

    Created model and view

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 
+from model import initialize_board, initialize_snake, set_new_position
+from view import draw
+
 step = 20
 width = 800
 height = 600
@@ -22,20 +25,12 @@ def turn(direction):
         return 1
     return direction
 
-def set_new_position(x_pos, y_pos, direction):
-    if direction == 2:
-        return (x_pos, y_pos + step)
-    if direction == 0:
-        return (x_pos, y_pos - step)
-    if direction == 3:
-        return (x_pos - step, y_pos)
-    if direction == 1:
-        return (x_pos + step, y_pos)
-    return (x_pos, y_pos)
-
-head_x = 400
-head_y = 300
+# head_x = 400 #tego nie potrzebujemy
+# head_y = 300 #tego nie potrzebujemy
 head_direction = 0
+board = initialize_board()
+snake = initialize_snake(board)
+# print(board) # jak chcesz zobaczyc gdzie jest
 
 while True:
 
@@ -43,13 +38,10 @@ while True:
         if event.type == pygame.QUIT:
             exit(1)
     head_direction = turn(head_direction)
-    head_x, head_y = set_new_position(head_x, head_y, head_direction)
-
+    set_new_position(head_direction,snake,board)
     screen.fill((255, 255, 255))
 
-    kwadrat = pygame.Rect(head_x, head_y, 20, 20)
-    pygame.draw.rect(screen, (128, 128, 128), kwadrat)
-
+    draw(board, screen)
     pygame.display.flip()
     counter = clock.tick(16)
 
---
+++ view.py
@@ -0,0 +1,15 @@
+import pygame
+
+def draw(board,screen):
+    for coordinates, value in board.items():
+        if value == "Snakehead":
+            head_x = coordinates[0] * 20 # 0 bo jest to od pierwszego elementu
+            head_y = coordinates[1] * 20
+            head_rect = pygame.Rect(head_x, head_y, 20, 20)
+            pygame.draw.rect(screen, (128, 128, 128),head_rect)
+        elif value is None:
+            head_x = coordinates[0] * 20  # 0 bo jest to od pierwszego elementu
+            head_y = coordinates[1] * 20
+            head_rect = pygame.Rect(head_x, head_y, 20, 20)
+            pygame.draw.rect(screen, (148, 170, 207), head_rect) # wartosci od zera do 255 red, gree, blue czyli RGB
+
